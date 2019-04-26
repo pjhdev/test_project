2b7acaca0ad74091bc696f1a9dca5b74fc4dabf5
박준호 <zkdlwnfm1@oozooinc.com>
2019-04-26 13:31:41 +0900

    [v0.13.0.0] force_ssl option to true

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TestProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Asia/Seoul'
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
